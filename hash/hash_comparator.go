009a9cc10d3bfaef29ee67efba737f17cfa9dccf
Sergey Eremenko <seremenko@wish.com>
2021-07-14 15:23:41 -0700

    pbkdf2-sha256 support

--- hash/hash_comparator.go
+++ hash/hash_comparator.go
@@ -2,10 +2,14 @@ package hash
 
 import (
 	"context"
+	"crypto/sha256"
 	"crypto/subtle"
 	"encoding/base64"
 	"fmt"
+	"golang.org/x/crypto/pbkdf2"
+	"hash"
 	"regexp"
+	"strconv"
 	"strings"
 
 	"github.com/pkg/errors"
@@ -22,6 +26,8 @@ func Compare(ctx context.Context, password []byte, hash []byte) error {
 		return CompareBcrypt(ctx, password, hash)
 	} else if IsArgon2idHash(hash) {
 		return CompareArgon2id(ctx, password, hash)
+	} else if IsPbkdf2Hash(hash) {
+		return ComparePbkdf2(ctx, password, hash)
 	} else {
 		return ErrUnknownHashAlgorithm
 	}
@@ -38,6 +44,32 @@ func CompareBcrypt(_ context.Context, password []byte, hash []byte) error {
 	}
 
 	return nil
+}
+
+func ComparePbkdf2(_ context.Context, password []byte, hash []byte) error {
+	parts := strings.Split(string(hash), "$")
+
+	if len(parts) != 5 {
+		return ErrInvalidHash
+	}
+
+	rawSalt, err := parseBase64(parts[3])
+	if err != nil {
+		fmt.Println(err)
+		return ErrInvalidHash
+	}
+
+	options := parseOptions(parts[2])
+
+	if iterations, ok := options["i"]; ok {
+		if verifyPBKDF2(sha256.New, string(password), iterations, rawSalt, parts[4]) {
+			return nil
+		}
+
+		return ErrMismatchedHashAndPassword
+	}
+
+	return ErrInvalidHash
 }
 
 func CompareArgon2id(_ context.Context, password []byte, hash []byte) error {
@@ -67,6 +99,11 @@ func IsBcryptHash(hash []byte) bool {
 
 func IsArgon2idHash(hash []byte) bool {
 	res, _ := regexp.Match("^\\$argon2id\\$", hash)
+	return res
+}
+
+func IsPbkdf2Hash(hash []byte) bool {
+	res, _ := regexp.Match("^\\$pbkdf2-sha256\\$", hash)
 	return res
 }
 
@@ -104,4 +141,54 @@ func decodeArgon2idHash(encodedHash string) (p *config.Argon2, salt, hash []byte
 	p.KeyLength = uint32(len(hash))
 
 	return p, salt, hash, nil
+}
+
+// copied from https://github.com/aykevl/pwhash/blob/master/pwhash.go#L115
+func parseBase64(s string) ([]byte, error) {
+	// Strip off trailing '=' chars.
+	s = strings.TrimRight(s, "=")
+
+	// Use standard encoding (not URL encoding).
+	s = strings.Replace(s, ".", "+", -1) // for MCF
+	s = strings.Replace(s, "-", "+", -1)
+	s = strings.Replace(s, "_", "/", -1)
+
+	return base64.RawStdEncoding.Strict().DecodeString(s)
+}
+
+// copied from https://github.com/aykevl/pwhash/blob/master/pwhash.go#L115
+// verifyPBKDF2 checks whether the given PBKDF2 hash is valid and returns true
+// iff the password matches the hash.
+func verifyPBKDF2(hashFunc func() hash.Hash, password string, iterations int, salt []byte, hash string) bool {
+	rawHash, err := parseBase64(hash)
+	if err != nil {
+		return false
+	}
+
+	key := pbkdf2.Key([]byte(password), salt, iterations, len(rawHash), hashFunc)
+	return subtle.ConstantTimeCompare(rawHash, key) == 1
+}
+
+// parseOptions parses an option string like "m=65536,t=1,p=4" into its
+// individual values: {"m": 65536, "t": 1, "p"=4}. A parse error results in a
+// nil map.
+func parseOptions(s string) map[string]int {
+	parts := strings.Split(s, ",")
+	opts := make(map[string]int)
+	for _, part := range parts {
+		index := strings.IndexByte(s, '=')
+		if index < 0 {
+			return nil
+		}
+		key := part[:index]
+		value, err := strconv.Atoi(part[index+1:])
+		if err != nil {
+			return nil
+		}
+		if _, ok := opts[key]; ok {
+			return nil // key already exists (invalid)
+		}
+		opts[key] = value
+	}
+	return opts
 }
--- hash/hasher_test.go
+++ hash/hasher_test.go
@@ -1,9 +1,12 @@
 package hash_test
 
 import (
+	"bufio"
 	"context"
 	"crypto/rand"
+	"encoding/json"
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -148,4 +151,46 @@ func TestCompare(t *testing.T) {
 	assert.Nil(t, hash.Compare(context.Background(), []byte("test"), []byte("$argon2id$v=19$m=32,t=5,p=4$cm94YnRVOW5jZzFzcVE4bQ$fBxypOL0nP/zdPE71JtAV71i487LbX3fJI5PoTN6Lp4")))
 	assert.Nil(t, hash.CompareArgon2id(context.Background(), []byte("test"), []byte("$argon2id$v=19$m=32,t=5,p=4$cm94YnRVOW5jZzFzcVE4bQ$fBxypOL0nP/zdPE71JtAV71i487LbX3fJI5PoTN6Lp4")))
 	assert.Error(t, hash.Compare(context.Background(), []byte("test"), []byte("$argon2id$v=19$m=32,t=5,p=4$cm94YnRVOW5jZzFzcVE4bQ$fBxypOL0nP/zdPE71JtAV71i487LbX3fJI5PoTN6Lp5")))
+}
+
+func TestTest(t *testing.T) {
+	assert.False(t, hash.IsPbkdf2Hash([]byte("$pbkdf2-sha512$i=10000$O484sW7giRw+nt5WVnp15w$jEUMVZ9adB+63ko/8Dr9oB1jWdndpVVQ65xRlT+tA1GTKcJ7BWlTjdaiILzZAhIPEtgTImKvbgnu8TS/ZrjKgA")))
+	assert.True(t, hash.IsPbkdf2Hash([]byte("$pbkdf2-sha256$i=10000$O484sW7giRw+nt5WVnp15w$jEUMVZ9adB+63ko/8Dr9oB1jWdndpVVQ65xRlT+tA1GTKcJ7BWlTjdaiILzZAhIPEtgTImKvbgnu8TS/ZrjKgA")))
+	assert.False(t, hash.IsPbkdf2Hash([]byte("$pbkdf2-sha1$i=10000$O484sW7giRw+nt5WVnp15w$jEUMVZ9adB+63ko/8Dr9oB1jWdndpVVQ65xRlT+tA1GTKcJ7BWlTjdaiILzZAhIPEtgTImKvbgnu8TS/ZrjKgA")))
+
+	assert.False(t, hash.IsPbkdf2Hash([]byte("$pbkdf2-sha1123$i=10000$O484sW7giRw+nt5WVnp15w$jEUMVZ9adB+63ko/8Dr9oB1jWdndpVVQ65xRlT+tA1GTKcJ7BWlTjdaiILzZAhIPEtgTImKvbgnu8TS/ZrjKgA")))
+	assert.False(t, hash.IsPbkdf2Hash([]byte("$pbkdf2-256$i=10000$O484sW7giRw+nt5WVnp15w$jEUMVZ9adB+63ko/8Dr9oB1jWdndpVVQ65xRlT+tA1GTKcJ7BWlTjdaiILzZAhIPEtgTImKvbgnu8TS/ZrjKgA")))
+}
+
+func TestTest2(t *testing.T) {
+	assert.Nil(t, hash.Compare(context.Background(),
+		[]byte("WC2Bgb4YEMmYXbD"),
+		[]byte("$pbkdf2-sha256$i=16000$UmqN8V4rhXBuTakVolRKSWltrXXunXMuhTDmnDPmXCsF4DznXEsJ4dy7l1JKqVVKaU2pNUbI.d87J0RIKUUIYaw1pjQGQAhhTMmZ875XqjXmXCvlHOM8J0TIWSslZGzt3TvHmNOa856zFqK0ljLmfE.JcY6xtlYK4TxnbE1pTakVwtibU2oNodTaG2PMuff.3xtjbE3pndPa23vvHaM0RgiB0BojRMg5Z.y91/ofY.w9p5SSUqqVMmYMwdibE6IUYkypNUbIuXeOcY4Rwvj/3xuDcE4JoXTuvRcCIESodY5xjrG2ds7Zm9N6753z3tv7f09pzRnDOGeMEcJ4b.29Fw$WZfa.Njy04CY4w526qwn7GZC1V8/9RidpXCEC0Oyle0"),
+	))
+	assert.Nil(t, hash.Compare(context.Background(),
+		[]byte("MyT3stPa55WoRD123"),
+		[]byte("$pbkdf2-sha256$i=16000$hVBqbW3NmbP23htDiLH2PkdIqZUypnSuFSKEMIZQai2l1FrL2fufcy7lXMs5R0ipNSaEUAoBwFgLwbh3LuVcy9mbs7Y25hxDKOU8h7AWIgTgnFPKmbN2ztk7pxQCoJQS4hzj3DvHmFOKEaLU2jvH.B/D2BuDMOY8h/AeoxQC4FxrTYnR.l9LKeU8Z8w5p3TOmbMWghBirLW29r73HmPs/b93DqH0/h.jdO6d0zrH.D8HgHDOOQcgJCSk9N67FwIgRGiNkVJKqRVirBVCCOF8r1VKybk3JgQAoNS6F0JojXGOkdKaM2bMmRNizLmXMubcW0uJsVYK4dybk/Ieo7RWCg$M7TqF6cv1vDHMtcVAWEqFNwd5QZQJBFVBdpP.aT7fOE"),
+	))
+	assert.Error(t, hash.Compare(context.Background(),
+		[]byte("MyT3stPa55WoRD123"),
+		[]byte("$pbkdf2-sha256$i=16000$hVBqbW3NmbP23htDiLH2PkdIqZUypnSuFSKEMIZQai2l1FrL2fufcy7lXMs5R0ipNSaEUAoBwFgLwbh3LuVcy9mbs7Y25hxDKOU8h7AWIgTgnFPKmbN2ztk7pxQCoJQS4hzj3DvHmFOKEaLU2jvH.B/D2BuDMOY8h/AeoxQC4FxrTYnR.l9LKeU8Z8w5p3TOmbMWghBirLW29r73HmPs/b93DqH0/h.jdO6d0zrH.D8HgHDOOQcgJCSk9N67FwIgRGiNkVJKqRVirBVCCOF8r1VKybk3JgQAoNS6F0JojXGOkdKaM2bMmRNizLmXMubcW0uJsVYK4dybk/Ieo7RWCg$M7TqF6cv1vDHMtcVAWEqFNwd5QZQJBFVBdpP.fail"),
+	))
+}
+
+func TestTest3(t *testing.T) {
+	file, err := os.Open("hashes.txt")
+	assert.NoError(t, err)
+	defer file.Close()
+
+	r := bufio.NewReader(file)
+	for i := 1; i <= 10000; i++ {
+		line, _, _ := r.ReadLine()
+		data := make([]string, 2)
+		err := json.Unmarshal(line, &data)
+		assert.NoError(t, err)
+		assert.Nil(t, hash.Compare(context.Background(),
+			[]byte(data[0]),
+			[]byte(data[1]),
+		))
+	}
 }
